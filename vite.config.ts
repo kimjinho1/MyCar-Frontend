d57e62d2e5866c055ac78dcd63484caf3873598b
kimjinho1 <rlawlsgh8113@naver.com>
2023-06-30 05:10:42 +0900

    chore: 주석 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import envCompatible from "vite-plugin-env-compatible";
 import svgr from "vite-plugin-svgr";
 
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envPrefix: "REACT_APP_",
-
-  plugins: [react(), envCompatible, svgr()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
