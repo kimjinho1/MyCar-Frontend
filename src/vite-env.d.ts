34e0ab9185929ef1ee12ffc445ab50c3f4f6a962
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-01 02:31:42 +0900

    chore: env 백엔드 url 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+interface ImportMetaEnv {
+  VITE_BACKEND_URL: string;
+  // ... more env variables if you have
+}
+
+interface ImportMeta {
+  env: ImportMetaEnv;
+}
