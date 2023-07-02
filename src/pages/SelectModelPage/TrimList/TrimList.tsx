9cc64a915d1f58a69b1e0c8d0878b7b0f3b1feb4
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-02 20:41:13 +0900

    feat: 트림 선택 컴포넌트 구현

---
+++ src/pages/SelectModelPage/TrimList/TrimList.tsx
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { trimInfosState } from "@/stores";
+import { useRecoilValue } from "recoil";
+import { routerPath } from "@/routes";
+
+export const TrimList = () => {
+  const navigate = useNavigate();
+
+  const trimInfos = useRecoilValue(trimInfosState);
+
+  const handleMakingModelButtonClick = (modelCode: string) => {
+    navigate(routerPath.getMakingModelPath(modelCode));
+  };
+
+  return (
+    <TrimListDiv>
+      <TrimListWrap>
+        {trimInfos.map((trimInfo) => {
+          return (
+            <TrimContainer>
+              <TrimInfoDiv>
+                <strong>{trimInfo.trimName}</strong>
+                <p>{trimInfo.modelPrice.toLocaleString()} 원</p>
+              </TrimInfoDiv>
+              <TrimImageDiv>
+                <img
+                  src={
+                    import.meta.env.VITE_BACKEND_URL + trimInfo.modelImagePath
+                  }
+                />
+              </TrimImageDiv>
+              <FilterSummaryDiv>
+                <p>{trimInfo.filterSummary}</p>
+              </FilterSummaryDiv>
+              <MakingModelButton
+                onClick={() => handleMakingModelButtonClick(trimInfo.modelCode)}
+              >
+                내 차 만들기
+              </MakingModelButton>
+            </TrimContainer>
+          );
+        })}
+      </TrimListWrap>
+    </TrimListDiv>
+  );
+};
+
+const TrimListDiv = styled.div`
+  width: 100%;
+  margin-left: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TrimListWrap = styled.div`
+  width: 100%;
+  margin: 5px 0px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const TrimContainer = styled.div`
+  padding: 25px 0;
+  width: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #f6f3f2;
+`;
+
+const TrimInfoDiv = styled.div`
+  margin-bottom: 30px;
+  width: 70%;
+
+  > strong {
+    font-size: 16px;
+  }
+
+  > p {
+    margin: 5px 0;
+    font-size: 13px;
+    font-weight: bolder;
+  }
+`;
+
+const TrimImageDiv = styled.div`
+  width: 85%;
+
+  > img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const FilterSummaryDiv = styled.div`
+  margin: 15px 0;
+  width: 70%;
+
+  > p {
+    margin: 0;
+    font-size: 12px;
+    color: #666;
+  }
+`;
+
+const MakingModelButton = styled.button`
+  margin-top: 15px;
+  width: 75%;
+  padding: 10px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: none;
+  background-color: #002c5f;
+  color: white;
+  font-size: 12px;
+  cursor: pointer;
+`;
