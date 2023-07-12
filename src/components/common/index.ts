43056322e10c6b4974ff3004db0b623961b82480
kimjinho1 <rlawlsgh8113@naver.com>
2023-07-12 09:07:41 +0900

    feat: 수평선 컴포넌트 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,7 @@
 export * from "./Logo";
 export * from "./VerticalLine";
+export * from "./HorizontalLine";
 export * from "./PopUpModal";
+export * from "./ModalConfirmButton";
 export * from "./Layout";
 export * from "./Footer";
